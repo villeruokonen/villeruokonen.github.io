0f7ac36bf5862b79ef5663be9677f288fb178366
Ville Ruokonen <ruokonenv@gmail.com>
2022-12-09 14:58:31 +0200

    added brickworld demo

--- js/js_main.js
+++ js/js_main.js
@@ -5,24 +5,17 @@ function handle_link(index) {
             window.open("about.html", "_parent");
             break;
         case 1:
+            window.open("../BrickWorldWebGL/index.html", "_parent");
+            break;
+        case 2:
             window.open("https://github.com/villeruokonen?tab=repositories");
             break;
-        case 2: //linktree link index
+        case 3: //linktree link index
             window.open("https://linktr.ee/villeruo");
             break;
-        case 3: //linktree link index
+        case 4: //linktree link index
             window.open("https://linkedin.com/in/villeruo");
             break;
-        case 4:
-            window.open("main_html.html");
-            break;
-        case 5:
-            window.open("https://docs.google.com/document/d/17xFSdhFWHwWpOze68DK5_Fn4H1kTlXuwNuOj4c91wXQ/edit?usp=sharing");
-            break;
-        case 6:
-            window.open("https://github.com/villeruokonen");
-            break;
-
     }
 }
 
