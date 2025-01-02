128ecb10a9231d49fff16f8c4c16dd77be9ce759
Ville Ruokonen <ruokonenv@gmail.com>
2025-01-02 22:50:54 +0200

    Move color dictionaries to separate file

---
+++ portfolio/src/technologyColors.ts
@@ -0,0 +1,25 @@
+const technologyColors: { [key: string]: string; } = {
+    'React': '#61DAFB',
+    'TypeScript': '#007ACC',
+    'JavaScript': '#F7DF1E',
+    'C#': '#68217A',
+    'C++': '#00599C',
+    'NodeJS': '#68A063',
+    'Unity': '#000000',
+    'Unreal': '#0E1128',
+    'OpenGL': '#5586A4',
+};
+
+const technologyAccentColors: { [key: string]: string } = {
+    'React': '#20232A',
+    'TypeScript': '#FFFFFF',
+    'JavaScript': '#000000',
+    'C#': '#FFFFFF',
+    'C++': '#FFFFFF',
+    'NodeJS': '#FFFFFF',
+    'Unity': '#FFFFFF',
+    'Unreal': '#FFFFFF',
+    'OpenGL': '#FFFFFF',
+};
+
+export { technologyColors, technologyAccentColors }
