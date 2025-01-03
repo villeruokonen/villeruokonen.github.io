f88c6c3cd7a0334b6dbc9e6187d9129f724d2a95
Ville Ruokonen <ruokonenv@gmail.com>
2025-01-03 13:09:47 +0200

    Create skillset display

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import ProjectList from './components/ProjectList';
 import Header from './components/Header'
+import SkillsetDisplay from './components/SkillsetDisplay';
 
 function App() {
   return (
     <>
       <Header/>
+      <SkillsetDisplay/>
       <main>
         <h2>Stuff I've done</h2>
         <ProjectList />
---
+++ portfolio/src/components/SkillsetDisplay.tsx
@@ -0,0 +1,36 @@
+import TechnologyBadge from "./TechnologyBadge";
+import { getProjects } from "../services/projectService";
+import { useEffect, useState } from "react";
+import './SkillsetDisplay.css'
+
+const SkillsetDisplay: React.FC = () => {
+    const [skills, setSkills] = useState<string[]>([]);
+
+    const loadSkills = async () => {
+        const projects = await getProjects();
+
+        const skills = projects
+            .flatMap(project => project.technologies)
+            .filter((tech, index, self) => self.indexOf(tech) === index)
+            .sort((a, b) => a.localeCompare(b))
+
+        setSkills(skills);
+    }
+
+    useEffect(() => {
+        loadSkills();
+    }, []);
+
+    return (
+        <div className="skill-container">
+            <h2>Current skills</h2>
+            <span className="skill-badges">
+                {skills.map(s => (
+                    <TechnologyBadge technologyName={s} />
+                ))}
+            </span>
+        </div>
+    );
+}
+
+export default SkillsetDisplay;
