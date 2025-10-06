eda6a8b3032bc908165fbfb1ca79f8acff08b136
Ville Ruokonen <ruokonenv@gmail.com>
2025-10-06 21:17:59 +0300

    Add RuoSaves

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -10,8 +10,8 @@ function App() {
       <Header />
       <main>
         <div className="blurb">
-          <p>Hey! My name is Ville. I'm a programmer and game developer with a passion for interactive systems, from multiplayer shooters to procedural worlds.</p>
-          <p>I'm always open to new experiences and I try to learn something new every day -- you'll usually find me tinkering with a new mechanic or technology, just for the fun of it.</p>
+          <p>Hey! My name is Ville. I'm a programmer and game developer with a passion for realtime interactions, from multiplayer shooters to procedural worlds.</p>
+          <p>I try to learn something new every day -- you'll usually find me tinkering with a new mechanic or technology, just for the fun of it.</p>
           <p>Get in touch and let's do something cool together!</p>
         </div>
         <h2>Skills</h2>
--- portfolio/src/components/PlatformEmblemGroup.tsx
+++ portfolio/src/components/PlatformEmblemGroup.tsx
@@ -1,11 +1,13 @@
-import { FaGithub, FaItchIo, FaSteam } from 'react-icons/fa6';
+import { FaGithub, FaGlobe, FaItchIo, FaSteam } from 'react-icons/fa6';
 import './PlatformEmblemGroup.css'
 
 const PlatformToEmblem: React.FC<{ platform: string }> = ({ platform }) => {
     const emblems: Record<string, JSX.Element> = {
-        steam: <FaSteam />,
-        itchio: <FaItchIo />,
-        github: <FaGithub />
+        'steam': <FaSteam />,
+        'itchio': <FaItchIo />,
+        'github': <FaGithub />,
+        'web': <FaGlobe />,
+        'unity-asset-store': <span className='unity-asset-store'><img width={'70px'} src='/3rd-party/unity_asset_store_logo.svg'></img></span>
     };
 
     return <div className="emblem">{emblems[platform]}</div>
