cbff8041fc18de80dcd87ff8c11f55aee388ac7f
Ville Ruokonen <ruokonenv@gmail.com>
2025-11-20 16:02:16 +0200

    Add headshot and labratory pic

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -9,13 +9,12 @@ function App() {
   return (
     <>
       <Header />
-      <SkillsetDisplay />
-      <main>
-        <h2>About me</h2>
-        <div className="blurb">
+      <div className="blurb">
           <p>Hey! My name is Ville. I'm a programmer and game developer with a passion for realtime interactions, from multiplayer shooters to procedural worlds.</p>
           <p>I try to learn something new every day -- you'll usually find me tinkering with a new mechanic or technology, just for the fun of it.</p>
         </div>
+      <SkillsetDisplay />
+      <main>
         <ShippedProductList />
         
         <ProjectList />
