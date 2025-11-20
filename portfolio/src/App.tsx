b032f9b6662f93c0265acedd0d560fd128b6ad8a
Ville Ruokonen <ruokonenv@gmail.com>
2025-11-20 16:03:58 +0200

    Remove redundant intro

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <Header />
       <div className="blurb">
-          <p>Hey! My name is Ville. I'm a programmer and game developer with a passion for realtime interactions, from multiplayer shooters to procedural worlds.</p>
+          <p>Hey! I'm a programmer and game developer with a passion for realtime interactions, from multiplayer shooters to procedural worlds.</p>
           <p>I try to learn something new every day -- you'll usually find me tinkering with a new mechanic or technology, just for the fun of it.</p>
         </div>
       <SkillsetDisplay />
